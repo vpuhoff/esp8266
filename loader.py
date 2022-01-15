6cda5d41be33e38695a8caa48d27a10baba7b762
vpuhoff <vpuhoff@live.ru>
2022-01-16 00:09:49 +0300

    Загрузка списка файлов через API GitHub

--- loader.py
+++ loader.py
@@ -6,11 +6,18 @@ except:
 import ujson
 import os
 import gc
+import btree
+try:
+    f = open("files_state", "r+b")
+except OSError:
+    f = open("files_state", "w+b")
+files_state = btree.open(f)
 
 def load_json(filename):
     with open(filename,"r") as f:
         data = ujson.loads(f.read())
     return data
+config = load_json("config.json")
 
 def exists(filename):
     try:
@@ -19,69 +26,49 @@ def exists(filename):
     except:
         return False
 
-def get_state(url, header_name='Content-Length'):
-    response = mrequests.head(url, save_headers=True)
-    for header in response.headers:
-        vars = header.decode().split(':', 1)
-        if vars[0].strip() == header_name:
-            del response
-            return vars[1]
-    del response
-    gc.collect()
-
-
-config = load_json("config.json")
-if not exists("files-state.json"):
-    files_state = {}
-else:
-    files_state = load_json("files-state.json")
-
 
 def load(url, filename, chunk_size=256):
-    existed = False
-    if mrequests:
-        remote_state = get_state(url)
-        local_state = files_state.get(filename, None)
-        print(local_state, remote_state)
-        if remote_state == local_state:
-            existed = True
-        else:
-            files_state[filename] = remote_state
-    if not existed:
-        response = urequests.get(url, stream=True)
-        chunk = b''
-        if response.status_code == 200:
-            with open(filename+".new", 'wb') as f:
-                while True:
-                    chunk = response.raw.read(chunk_size)
-                    if not chunk:
-                        break
-                    f.write(chunk)
-            if exists(filename):
-                os.remove(filename)
-            os.rename(filename+".new", filename)
-            print("File " + filename + " download success!: ", response.status_code)
-        else:
-            print("File " + filename + " download failed!: ", response.status_code)
-        del response
-        gc.collect()
-        return True
+    response = urequests.get(url, stream=True)
+    chunk = b''
+    if response.status_code == 200:
+        with open(filename+".new", 'wb') as f:
+            while True:
+                chunk = response.raw.read(chunk_size)
+                if not chunk:
+                    break
+                f.write(chunk)
+        if exists(filename):
+            os.remove(filename)
+        os.rename(filename+".new", filename)
+        print("File " + filename + " download success!: ", response.status_code)
     else:
-        print("File " + filename + " already up to date!: ")
-        return False
-    
+        print("File " + filename + " download failed!: ", response.status_code)
+    del response
+    gc.collect()
+    return True
 
-def save_state():
-    with open("files-state.json" ,"wb") as f:
-        ujson.dump(files_state, f)
-        print("Files state saved success")
+def get_files_list(username, repo):
+    list_url = "https://api.github.com/repos/"+username+"/"+repo+"/git/trees/master?recursive=1"
+    response = urequests.get(list_url, stream=True, headers={'User-Agent': 'request'})
+    sha = response.raw.read(47).decode().split(':')[1][1:]
+    if 'files_state' in files_state:
+        if sha == files_state:
+            print("Files already up to date")
+            return False
+        else:
+            files_state['files_state'] = sha
+            files_state.flush()
+    else:
+        files_state['files_state'] = sha
+        files_state.flush()
+    print("File list changed, loading files list..")
+    load(list_url, "file-list.json")
+    print("File list downloaded")
+    return True
 
 def update():
-    new_files = False
-    for listname in ["requirements","modules"]:
+    result = get_files_list(config['username'], config['repo'])
+    for listname in ["requirements"]:
         for k, v in config[listname].items():
             result = load(v,k)
-            if result:
-                new_files = True
-    if new_files:
-        save_state()
+
