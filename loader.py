27fe32700685f275f265115f42ca234c60996486
vpuhoff <vpuhoff@live.ru>
2022-01-15 10:59:07 +0300

    Доработка загрузчика

--- loader.py
+++ loader.py
@@ -1,7 +1,12 @@
 import urequests
+import ujson
 
 def test_load(chunk_size=256):
-    response = urequests.get('http://jsonplaceholder.typicode.com/albums/1', stream=True)
+    headers = {'content-type': 'application/json'}
+    # response = urequests.get('https://raw.githubusercontent.com/vpuhoff/esp8266/master/loader.py', headers=headers, stream=True)
+    # if response.status_code == 200:
+    #     print(response.headers['last-modified'])
+    response = urequests.get('https://raw.githubusercontent.com/SpotlightKid/mrequests/master/mrequests.py', stream=True)
     if response.status_code == 200:
         with open('loaded.dat', 'wb') as f:
             #while True:
@@ -9,5 +14,7 @@ def test_load(chunk_size=256):
             #    if not chunk:
             #        break
             f.write(response.content)
+        print("File download success!: ", response.status_code)
     else:
         print("File download failed!: ", response.status_code)
+
