4da4c042bddab1206727c1fe30e6821d58f7d544
vpuhoff <vpuhoff@live.ru>
2022-01-16 23:59:28 +0300

    test

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from time import sleep
 print("asdasd")
 print("zxc as ds ")
 print("asdasd ds ")
+print("asdasd ds ")
+print("asdasd ds ")
+print("asdasd ds ")
+print("asdasd ds ")
+print("asdasd ds ")
